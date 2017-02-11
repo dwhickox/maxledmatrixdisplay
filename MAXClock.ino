11dfedfcb2c1d85719dcd363d677baccba4aa901
dwhickox <dwhickox@users.noreply.github.com>
2017-02-10 23:34:39 -0600

    replaced delay() with millis() to fix slow down
    I was experiencing about a quarter second slow down per minute, which is something I was afraid of with a clock, but I wanted to see how affected the program would be by just the delay function, and I decided a lot since it was easily noticeable, and the clock speed is not that high so it is orders of magnitude higher than the noticeability of the same thing on a modern processor. So the long and the short of it is a millis() if loop now replaces delay(), it should also have rollover protection but that is yet to be seen.

--- MAXClock.ino
+++ MAXClock.ino
@@ -1,16 +1,17 @@
 /*copyright David Hickox
- * Requirements to run, adafruit gfx and Max72xxPanel by markruys https://github.com/markruys/arduino-Max72xxPanel
- * 
- * 
- * variables for you to mess with:
- * pinCS, set this to what digital pin you want to connect CS (on the matrix input) to
- * numHorizDisp, set how many 8*8 max matricies you have horizontaly
- * numVertDisp, set how many 8*8 max matricies you have verticaly
- * ampm, change this from 0 to 1 if you want 12 hour time instead of 24
- * txtsz, this is the text size, mess with this if you have more pannels or are just curious
- * rot, (0-3) rotates your display oreintation depending on how you have it set up, the default and most common is 1
- * 
- */
+   set the clock with ab hours cd min and ef seconds in the format "a,b,c,d,e,f" in the serial console
+   Requirements to run, adafruit gfx and Max72xxPanel by markruys https://github.com/markruys/arduino-Max72xxPanel
+
+
+   variables for you to mess with:
+   pinCS, set this to what digital pin you want to connect CS (on the matrix input) to
+   numHorizDisp, set how many 8*8 max matricies you have horizontaly
+   numVertDisp, set how many 8*8 max matricies you have verticaly
+   ampm, change this from 0 to 1 if you want 12 hour time instead of 24
+   txtsz, this is the text size, mess with this if you have more pannels or are just curious
+   rot, (0-3) rotates your display oreintation depending on how you have it set up, the default and most common is 1
+   interval, i guess if you wanted to use this as a metrinome or something you could change the lenght of the second with this
+*/
 
 
 #include <Adafruit_GFX.h>
@@ -30,26 +31,41 @@ int m1;
 int s;
 int s1;
 int curs = 0;
+int interval = 1000;
+int debug;
+int i;
 String msg;
+unsigned long prevMil = 0;
+
 Max72xxPanel matrix = Max72xxPanel(pinCS, numHorizDisp, numVertDisp);
 
 void setup() {
   Serial.begin(57600);
   matrix.setIntensity(4); // Set brightness between 0 and 15
 
-  matrix.setRotation(0,rot);//you may have to change this section depending on your LED matrix setup
-  matrix.setRotation(1,rot);
-  matrix.setRotation(2,rot);
-  matrix.setRotation(3,rot);
+  matrix.setRotation(0, rot); //you may have to change this section depending on your LED matrix setup
+  matrix.setRotation(1, rot);
+  matrix.setRotation(2, rot);
+  matrix.setRotation(3, rot);
   matrix.setTextSize(txtsz);
-  matrix.setTextWrap(false); 
+  matrix.setTextWrap(false);
   matrix.setTextColor(HIGH);
   Serial.println("made it through setup");
 }
 
 void loop() {
-  if (Serial.available() > 5) // set the clock with ab hours cd min and ef seconds in the format "a,b,c,d,e,f" in the serial console
-  {
+  unsigned long curMil = millis();
+  if ((unsigned long)(curMil - prevMil) >= interval) {
+    if ((unsigned long)(curMil - prevMil) > interval){
+      debug = debug + ((curMil - prevMil) - interval);
+      i++;
+      if (i == 10){
+        Serial.println(debug);
+        i = 0;
+      }
+    }
+    if (Serial.available() > 5) // set the clock with ab hours cd min and ef seconds in the format "a,b,c,d,e,f" in the serial console
+    {
       Serial.println("serial was deamed avialable");
       h = Serial.parseInt();
       Serial.println(h);
@@ -63,61 +79,62 @@ void loop() {
       Serial.println(s);
       s1 = Serial.parseInt();
       Serial.println(s1);
-  }
-  
-  matrix.fillScreen(LOW);//Empty the screen
-  matrix.setCursor(curs,0);//Move the cursor to the end of the screen
-  msg = "";
-  msg.concat(h);
-  msg.concat(h1);
-  msg.concat(":");
-  msg.concat(m);
-  msg.concat(m1);
-  matrix.print(msg);
-  msg.concat(":");
-  msg.concat(s);
-  msg.concat(s1);
-  Serial.println(msg);
-  matrix.write();
-  delay(1000);
-  s1++;
-  if (s1 >= 10){
-    s1 = 0;
-    s++;
-    Serial.println("seconds reset");
-  }
-  if (s >= 6){
-    s = 0; 
-    m1++;
-    Serial.println("10 seconds reset");
-  }
-  if (m1 >= 10){
-    m1 = 0;
-    m++;
-    Serial.println("minutes reset");
-  }
-  if (m >= 6){
-    m = 0;
-    h1++;
-    Serial.println("10 minutes reset");
-  }
-  if ((ampm == 0 and h1 >= 10 and h < 2) or (ampm ==1 and h1 >= 10 and h < 1)){
-    h1 = 0;
-    h++;
-    Serial.println("hours reset");
-  }
-  if (ampm == 0){
-    if (h >= 2 and h1 >= 4){
-      h = 0;
-      h1 = 0;
-    Serial.println("24 hour turn over reset");
     }
-  }
-  else{
-    if (h >= 1 and h1 >= 4){
-      h = 0;
-      h1 = 0;
-    Serial.println("12 hour turn over reset");
+
+    matrix.fillScreen(LOW);//Empty the screen
+    matrix.setCursor(curs, 0); //Move the cursor to the end of the screen
+    msg = "";
+    msg.concat(h);
+    msg.concat(h1);
+    msg.concat(":");
+    msg.concat(m);
+    msg.concat(m1);
+    matrix.print(msg);
+    msg.concat(":");
+    msg.concat(s);
+    msg.concat(s1);
+    Serial.println(msg);
+    matrix.write();
+    s1++;
+    if (s1 >= 10) {
+      s1 = 0;
+      s++;
+      Serial.println("seconds reset");
     }
+    if (s >= 6) {
+      s = 0;
+      m1++;
+      Serial.println("10 seconds reset");
+    }
+    if (m1 >= 10) {
+      m1 = 0;
+      m++;
+      Serial.println("minutes reset");
+    }
+    if (m >= 6) {
+      m = 0;
+      h1++;
+      Serial.println("10 minutes reset");
+    }
+    if ((ampm == 0 and h1 >= 10 and h < 2) or (ampm == 1 and h1 >= 10 and h < 1)) {
+      h1 = 0;
+      h++;
+      Serial.println("hours reset");
+    }
+    if (ampm == 0) {
+      if (h >= 2 and h1 >= 4) {
+        h = 0;
+        h1 = 0;
+        Serial.println("24 hour turn over reset");
+      }
+    }
+    else {
+      if (h >= 1 and h1 >= 4) {
+        h = 0;
+        h1 = 0;
+        Serial.println("12 hour turn over reset");
+      }
+    }
+    prevMil = curMil;
   }
 }
